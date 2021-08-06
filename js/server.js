de9c14900253af7164c38f39c24cc5d66a7ba1f3
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-06 09:17:53 -0600

    JS file to render data in terminal and create Node.js server

--- js/server.js
+++ js/server.js
@@ -1,3 +1,11 @@
+/**************************************/
+/* Name: Keith Francis
+/* Course: ICT 4570
+/* Date: Aug 6, 2021
+/* Notes: A basic JS file to retrieve JSON data from a JSON file,
+/* convert it to a JS object and render to terminal, and create a 
+/* Node.js server and render a HTML page.
+/**************************************/
 const { createServer } = require('http');
 const fs = require('fs');
 const port = 3000;
@@ -6,13 +14,17 @@ const port = 3000;
 const jsonData = require('./products.json');
 const productData = jsonData.Men;
 
+// Render JSON data outside object format
 for (item in productData) {
   if (productData[item].sleeve === 'short') {
     console.log(productData[item].sleeve);
-    
+    console.log(productData[item].size);
+    console.log(productData[item].colors + '\n');
   }
   if (productData[item].sleeve === 'long') {
     console.log(productData[item].sleeve);
+    console.log(productData[item].size);
+    console.log(productData[item].colors  + '\n');
   }
 } 
 
@@ -24,6 +36,7 @@ createServer((req, res) => {
       res.writeHead(404);
       res.write('HTML file not found!');
     } else {
+      // Render HTML file
       res.writeHead(200, {'Content-Type' : 'text/html'});
       res.write(data);
       return res.end();
